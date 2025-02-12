6a9884733adcf66255849163abb4ed6cb02192ee
onyenaucheya emmanuel <onyenaucheyaemma@gmail.com>
2025-02-12 18:06:44 +0000

    pushing to git

--- onlineretailpos/settings/devlopement.py
+++ onlineretailpos/settings/devlopement.py
@@ -1,4 +1,4 @@
-from .base import *
+from .settings import *
 import os, socket
 # from sshtunnel import SSHTunnelForwarder
 from dotenv import load_dotenv
--- onlineretailpos/settings/settings.py
+++ onlineretailpos/settings/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = (os.path.join(PROJECT_DIR,'static'),)
 # ASSETS_ROOT = Path.joinpath(STATIC_ROOT,'assets/')
 
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
